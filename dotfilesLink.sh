ffb2f187bd2c4ade07c4bcee8a78e32e5baf5b71
kubor <rkubo.4w@gmail.com>
2015-01-15 15:23:12 +0900

    add config for submodule

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+# sync git submodule(for vim-neocomplete)
+git submodule init
+git submodule update
