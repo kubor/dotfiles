1fa6352e1473dfcce376182139e3c634f0dd6952
K-Ryuichi <rkubo.4w@gmail.com>
2015-11-07 13:09:08 +0900

    add ln for snippets

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "make symbolic links to $HOME"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.peco.snippet.bioinfo ~/.peco.snippet.bioinfo
 # sync git submodule(for vim-neocomplete)
 git submodule init
 git submodule update
